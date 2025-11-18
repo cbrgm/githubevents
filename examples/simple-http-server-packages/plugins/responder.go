e00d8dfe8e8a463a3a551b394f176ae5797d6d55
cbrgm <chris@cbrgm.net>
2025-11-18 20:20:54 +0100

    deps: update to go github v79 everywhere

--- examples/simple-http-server-packages/plugins/responder.go
+++ examples/simple-http-server-packages/plugins/responder.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"github.com/cbrgm/githubevents/v2/githubevents"
-	"github.com/google/go-github/v78/github"
+	"github.com/google/go-github/v79/github"
 )
 
 func NewResponder(msg string) githubevents.IssueCommentEventHandleFunc {
