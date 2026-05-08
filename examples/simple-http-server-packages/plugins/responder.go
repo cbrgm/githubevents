281fab5ebc46f3e411aacf7f1be1cee89353eea1
cbrgm <chris@cbrgm.net>
2026-05-08 11:31:43 +0200

    update to go-github v86 everywhere

--- examples/simple-http-server-packages/plugins/responder.go
+++ examples/simple-http-server-packages/plugins/responder.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"github.com/cbrgm/githubevents/v2/githubevents"
-	"github.com/google/go-github/v85/github"
+	"github.com/google/go-github/v86/github"
 )
 
 func NewResponder(msg string) githubevents.IssueCommentEventHandleFunc {
